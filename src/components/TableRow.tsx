9e151dac11193ed907e9da780bec8051cb925ab3
Antoni0o <antonio.dias8997@gmail.com>
2023-07-29 22:41:57 -0300

    table row component styles

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -1,13 +1,15 @@
+import styles from "./tableRow.module.css";
+
 type ITableRowProps = {
   name: string;
 }
 
 export default function TableRow({ name }: ITableRowProps) {
   return (
-    <tr>
-      <td>{name}</td>
-      <td><span>Editar</span></td>
-      <td><span>Deletar</span></td>
+    <tr className={styles.row}>
+      <td className={styles.nameArea}>{name}</td>
+      <td><span className={styles.editButton}>Editar</span></td>
+      <td><span className={styles.deleteButton}>Deletar</span></td>
     </tr>
   );
 }
